8528681b10211850a921f322935f9559c89cc8cf
devarshi-007 <devarshi.trivedi@improwised.com>
2023-12-17 12:12:27 +0530

    exercise-1 template

--- back/process_movie.php
+++ back/process_movie.php
@@ -10,7 +10,12 @@
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         // Retrieve movie data from the POST request
+
         // here example given for watched
+        /* Exercise-1:
+           - Define the variables to catch $_POST['movieName'] and $_POST['genre']
+           - Print those variables in Movie saved successfully! message
+        */
         $watched = $_POST["watched"];
 
         // Simple validation
@@ -19,7 +24,8 @@
             header("refresh:2;url=../pages/add.php");
             exit();
         } else {
-            echo '<div class="message-container">Movie saved successfully!</div>';
+            echo '<div class="message-container">Movie saved successfully!
+            <br><br> Movie Name: ' . /* Movie Name */  '<br>Genre: ' . /* Genre */ '</div>';
             header("refresh:2;url=../index.php");
             exit();
         }
