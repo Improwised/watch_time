92ed8566983efe264241b555dd29a38d3101a4d1
devarshi-007 <devarshi.trivedi@improwised.com>
2023-12-17 12:31:23 +0530

    exercise 2 & 3 template added

--- index.php
+++ index.php
@@ -14,16 +14,65 @@
                 <h2 class="light-gray">Watch List</h2>
                 <a href='pages/add.php' class='item-button'><span>Add Item</span></a>
             </div>
-            <?php 
-            
-            
-            ?>
+                        
+            <div class="movie-cards-container">
+                <?php
+                /* Exercise-2: Define array called movie
+                   - Movied should Associative array and it shoud contains id, name, genre as key
+                   - If you want any help demo is given for watched section
+                */
+
+                // Loop 
+                /* Exercise-3: 
+                   - Rename movie to movies and make movie as array of movie associative arrays
+                   - Create foreach loop on that array
+                   - demo is given for watched section
+                */
+                // open the foreach loop 
+                    if (isset($movie['name']) || isset($movie['genre'])){
+                        echo '<div class="movie-card">
+                        <div class="movie-card-header">
+                            <i class="fas fa-pencil-alt"></i>
+                        </div>
+                        <div class="movie-card-content">
+                            <h3>' . /* Write Movie Name*/ '</h3>
+                            <p>Genre: '. /* Write Movie Genre*/ '</p>
+                        </div>
+                        <div class="movie-card-buttons">
+                            <button class="watched-btn">Watched</button>
+                            <button class="delete-btn">Delete</button>
+                        </div>
+                        </div>';
+                    }
+                // close the for loop
+                ?>
+            </div>
         </div>
         <div class="half-width-wib"><h2 class="light-gray">Already Watched</h2>
-            <?php
+        <div class="movie-cards-container">
+                <?php
+                $movies = [
+                    ["id"=>1, "name" => "Pirates of The carrabian", "genre" => "Adventure"],
+                    ["id"=>2, "name" => "One Piece", "genre" => "Anime"]
+                ];
 
-
-            ?>
+                foreach ($movies as $movie) {
+                    echo '<div class="movie-card">
+                    <div class="movie-card-header">
+                        <i class="fas fa-pencil-alt"></i>
+                    </div>
+                    <div class="movie-card-content">
+                        <h3>' . $movie["name"] . '</h3>
+                        <p>Genre: ' . $movie["genre"]. '</p>
+                    </div>
+                    <div class="movie-card-buttons">
+                        <button class="watched-btn">Unwatch</button>
+                        <button class="delete-btn">Delete</button>
+                    </div>
+                    </div>';
+                }
+                ?>
+            </div>
         </div>
     </div>
     <!-- <?php include "inc/footer.php";?> -->
