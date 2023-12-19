5311525ed576ce8b1f2194c72ce49f9dfa31a628
devarshi-007 <124968745+devarshi-007@users.noreply.github.com>
2023-12-19 07:46:25 +0530

    Update delete.php
    Signed-off-by: devarshi-007 <124968745+devarshi-007@users.noreply.github.com>

--- back/delete.php
+++ back/delete.php
@@ -4,20 +4,8 @@
     if (isset($_GET['id']) && !empty($_GET['id'])) {
         $id = $_GET['id'];
         
-        // Implement your database connection and query here
-        // Example: $response = deleteItem($id);
-        
         //____try to uncomment me Exercise - 9____________________
         // deleteItem($id);
-
-        // if($response) {
-        //     echo "<script>alert(\"Deleted\")</script>";
-        //     header("refresh:0; ../index.php");
-        // } else {
-        //     echo "<script>alert(\"Error\")</script>";
-        //     header("refresh:0; ../index.php");
-        //     exit();
-        // }
         
     } else {
         // Redirect to the homepage or display an error message
